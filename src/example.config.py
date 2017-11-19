c3d19fdf0dd7869ec6f27259bc8c19527630c8a5
Thomas Marcon <tehem@tehem.net>
2017-11-19 20:06:01 +0100

    Remove ifconfig part of the config file
    - Using a new providers.json configuration file

--- src/example.config.py
+++ src/example.config.py
@@ -28,18 +28,3 @@ subdomains = ["subdomain1", "subdomain2", "subdomain3"]
 
 #300 seconds = 5 minutes
 ttl = '300'
-
-''' 
-IP address lookup service 
-run your own external IP provider:
-+ https://github.com/mpolden/ipd
-+ <?php $ip = $_SERVER['REMOTE_ADDR']; ?>
-  <?php print $ip; ?>
-e.g. 
-+ https://ifconfig.co
-+ http://ifconfig.me/ip
-+ http://whatismyip.akamai.com/
-+ http://ipinfo.io/ip
-+ many more ...
-'''
-ifconfig = 'choose_from_above_or_run_your_own'
